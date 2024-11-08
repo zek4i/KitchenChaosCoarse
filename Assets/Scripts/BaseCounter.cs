469122de2393417d44b34eceb94169036e4bebe9
Sania Tazeen <sania.tazeen04@gmail.com>
2024-11-08 15:16:02 +0530

    container counter complete

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -2,10 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCounter : MonoBehaviour
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
 {
+    [SerializeField] private Transform counterTopPoint;
+
+
+    private KitchenObject kitchenObject;
     public virtual void Interact(PlayerController player)
     {
         Debug.LogError("BaseCounter.Interact();");
+    }
+
+    public Transform GetKitchenObjectFollowtransform()
+    {
+        return counterTopPoint;
+    }
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+    public bool HasKitchenObject() //checking if this counter has anything on top
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,48 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
 
-
-    private KitchenObject kitchenObject;
     
     public override void Interact(PlayerController player)
     {
-        if (kitchenObject == null) //if there is no kitchenObject existing
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this); //making code more clear
-           //kitchenObjectTransform.localPosition = Vector3.zero; //making sure that the obj appears on the right point
-
-        }
-        else
-        {
-            //give the object to the player
-            kitchenObject.SetKitchenObjectParent(player); //giving parent to the player
-        }
+       
         
     }
-    public Transform GetKitchenObjectFollowtransform()
-    {
-        return counterTopPoint;
-    }
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-    public bool HasKitchenObject() //checking if this counter has anything on top
-    {
-        return kitchenObject != null;
-    }
+
 }
